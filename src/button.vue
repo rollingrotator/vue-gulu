d907fd0e0b9142ed362ce4a133c667215ac09925
ping <513011631@qq.com>
2019-04-02 23:43:39 +0800

    添加iconfont 设置icon位置

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button class="d-button">加油啊,路飞</button>
+  <button class="d-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg class="icon" aria-hidden="true" v-if="icon">
+      <use :xlink:href=`#icon-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 </script>
 
 <style lang="scss">
@@ -14,6 +23,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -23,7 +36,20 @@
     &:focus {
       outline: none;
     }
-
+    > .icon {
+      order: 1;
+    }
+    > .content {
+      order: 2;
+    }
+    &.icon-right {
+      > .icon {
+        order: 2;
+      }
+      > .content {
+        order: 1;
+      }
+    }
   }
 
 </style>
