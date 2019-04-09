e0917cc6daca43f6454554e02fc0a2c7f5c55a9a
ping <513011631@qq.com>
2019-04-09 23:17:35 +0800

    添加d-button-group

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import Button from './button'
-import  Icon from './icon'
-Vue.component('d-button',Button)
-Vue.component('d-icon',Icon)
+import Icon from './icon'
+import ButtonGroup from './button-group'
+
+Vue.component('d-button', Button)
+Vue.component('d-icon', Icon)
+Vue.component('d-button-group', ButtonGroup)
 new Vue({
-  el:'#app',
-  data:{
-    loading1:false,
-    loading2:true,
-    loading3:false
+  el: '#app',
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false
   }
 });
 
---
+++ src/button-group.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "button-group"
+  }
+</script>
+
+<style lang="scss">
+  .d-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+  }
+</style>
