19d141797ec2c785fa08bfe10cd338e90f013377
ping <513011631@qq.com>
2019-04-10 18:45:32 +0800

    修改ignore

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-Vue.component('d-button', Button)
-Vue.component('d-icon', Icon)
-Vue.component('d-button-group', ButtonGroup)
+Vue.component('d-button', Button);
+Vue.component('d-icon', Icon);
+Vue.component('d-button-group', ButtonGroup);
 new Vue({
   el: '#app',
   data: {
