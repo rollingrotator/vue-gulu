d62475766a9726cb2969ecc8628d68b3f7a01590
ping <513011631@qq.com>
2019-04-11 00:01:27 +0800

    在页面挂载新元素；chai.js 测试

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import ButtonGroup from './button-group'
 Vue.component('d-button', Button);
 Vue.component('d-icon', Icon);
 Vue.component('d-button-group', ButtonGroup);
+
 new Vue({
   el: '#app',
   data: {
@@ -15,3 +16,14 @@ new Vue({
   }
 });
 
+import chai from 'chai'
+const  expect = chai.expect
+const Constructor = Vue.extend(Button)
+const button = new Constructor({
+  propsData:{
+    icon:'setting'
+  }
+})
+button.$mount('#test')
+const assert = button.$el.querySelector('use').getAttribute('xlink:href')
+expect(assert).to.eq('#icon-setting')
